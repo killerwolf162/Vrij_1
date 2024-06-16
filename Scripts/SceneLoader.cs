7095f31ecc0d3c6ff4aa5aa761ed1f01256e673d
Main_PC_Tom <tjongman.123@gmail.com>
2024-06-16 17:45:31 +0200

    sceneLoader aanpassing

--- Scripts/SceneLoader.cs
+++ Scripts/SceneLoader.cs
@@ -7,7 +7,7 @@ public class SceneLoader : MonoBehaviour
 {
     public void loadMainScene()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
     public void loadDeatchScene()
     {
